217385303ca3450d5dcf727d4ac3ad838fbade5b
Vitor Carrilho <vitor.carrilho@hotmail.com>
2020-05-07 05:09:22 -0300

    alterações

--- js/main.js
+++ js/main.js
@@ -24,4 +24,13 @@
         }
       
       });
+
+      const items = document.querySelectorAll(".faq a");
+
+      function toggleAccordion(){
+        this.classList.toggle('active');
+        this.nextElementSibling.classList.toggle('active');
+      }
+
+      items.forEach(item => item.addEventListener('click', toggleAccordion));
 })();
